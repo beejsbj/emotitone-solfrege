ce937f60cb8e17cac7d8d23ba3f920b71690f785
beejsbj <burooj.bj@gmail.com>
2026-08-18 20:55:37 +0530

    Restore app and route style guide

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <StyleGuide />
+  <StyleGuide v-if="isStyleGuide" />
+  <MainApp v-else />
 </template>
 
 <script setup lang="ts">
+import MainApp from "./MainApp.vue";
 import StyleGuide from "./style-guide/StyleGuide.vue";
+
+const pathname = window.location.pathname.replace(/\/+$/, "") || "/";
+const isStyleGuide = pathname === "/style-guide";
 </script>
